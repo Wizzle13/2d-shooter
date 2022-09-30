ab67c525654a298fa85bf7a7699f2bac85d71baf
CBurton <burton.chris13@gmail.com>
2022-09-30 13:58:47 -0500

    created style sheet

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function(){
+    // canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx= canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+    class Projectile {
+
+    }
+    class Praticle {
+
+    }
+    class Player {
+
+    }
+    class Enemy {
+
+    }
+    class Layer {
+
+    }
+    class Background{
+
+    }
+
+});
